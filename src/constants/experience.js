d66f0ec9f8e5562db8547295b1a37c0c5b6f77ab
Khalid Ansari <khaliljpc@gmail.com>
2024-03-30 14:25:33 +0530

    add: new skills

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -1,6 +1,7 @@
 const experience = [
   { skillName: 'React', skillLink: 'https://react.dev/' },
   { skillName: 'JavaScript', skillLink: 'https://javascript.info/' },
+  { skillName: 'TypeScript', skillLink: 'https://www.typescriptlang.org/' },
   { skillName: 'HTML', skillLink: '/', light: true },
   { skillName: 'CSS', skillLink: '/', light: true },
   { skillName: 'Tailwind', skillLink: 'https://tailwindcss.com/' },
@@ -10,6 +11,7 @@ const experience = [
   { skillName: 'React Query', skillLink: 'https://tanstack.com/query/v3/' },
   { skillName: 'Styled Components', skillLink: 'https://styled-components.com/' },
   { skillName: 'Form Hook', skillLink: 'https://www.react-hook-form.com/' },
+  { skillName: 'Zod', skillLink: 'https://zod.dev/' },
 ];
 
 export default experience;
